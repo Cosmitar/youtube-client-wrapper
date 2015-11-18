3f29345394ea87202a5259cb4b2b1e5e72ae3ef1
Cosmitar <mhherrera31@gmail.com>
2015-11-18 04:08:59 -0300

    add readme doc about authorization

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import YouTubeChannel from './entities/YouTubeChannel';
 import YouTubePlaylist from './entities/YouTubePlaylist';
 import YouTubeSearch from './entities/YouTubeSearch';
 import YouTubeConfig from './services/Config';
+import YouTubeAuth from './services/Auth';
 
 export default {
     Video: YouTubeVideo,
     Channel: YouTubeChannel,
     Playlist: YouTubePlaylist,
     Search: YouTubeSearch,
-    Config: YouTubeConfig
+    Config: YouTubeConfig,
+    Auth: YouTubeAuth
 };
